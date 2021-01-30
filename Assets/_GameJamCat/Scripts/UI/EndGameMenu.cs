b09ee0e0082de1518845fef12569a0365f68bcb9
Gursaanj <gursaanjresides@gmail.com>
2021-01-29 23:29:17 -0800

    UpdatedLogic

--- Assets/_GameJamCat/Scripts/UI/EndGameMenu.cs
+++ Assets/_GameJamCat/Scripts/UI/EndGameMenu.cs
@@ -26,6 +26,24 @@ namespace GameJamCat
             _foundCat = false;
             StateManager.Instance.SetState(State.EndGame);
         }
+        
+        public void DisplayEndPanel(bool foundCat)
+        { 
+            if (_foundCat)
+            {
+                if (_winScreen != null)
+                {
+                    _winScreen.SetActive(true);
+                }
+            }
+            else
+            {
+                if (_loseScreen != null)
+                {
+                    _loseScreen.SetActive(true);
+                }
+            }
+        }
 
         private void Awake()
         {
@@ -39,35 +57,16 @@ namespace GameJamCat
                 _winScreen.SetActive(false);
             }
 
-            StateManager.Instance.OnStateChanged += DisplayEndPanel;
-            _loseReplayButton.onClick.AddListener(() => ResetGame());
-            _winReplayButton.onClick.AddListener(() => ResetGame());
-        }
-
-        private void OnDestroy()
-        {
-            StateManager.Instance.OnStateChanged -= DisplayEndPanel;
-        }
-
-        private void DisplayEndPanel(State state)
-        {
-            if (state == State.EndGame)
+            if (_loseReplayButton != null)
             {
-                if (_foundCat)
-                {
-                    if (_winScreen != null)
-                    {
-                        _winScreen.SetActive(true);
-                    }
-                }
-                else
-                {
-                    if (_loseScreen != null)
-                    {
-                        _loseScreen.SetActive(true);
-                    }
-                }
+                _loseReplayButton.onClick.AddListener(() => ResetGame());
             }
+
+            if (_winReplayButton != null)
+            {
+                _winReplayButton.onClick.AddListener(() => ResetGame());   
+            }
+            
         }
 
         private void ResetGame()
