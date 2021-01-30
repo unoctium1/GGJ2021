3638cfa885b4bae4359a7494e0412c74bbbcad44
AbelW <unoctium1@gmail.com>
2021-01-30 04:03:53 -0800

    Update cat manager to deal with cat generator. Not sure what the best way to deal with the ICatFactory part is, as my solution here seems like it might have gone against Martin's original intention. Open to changing it

--- Assets/_GameJamCat/Scripts/Managers/CatManager.cs
+++ Assets/_GameJamCat/Scripts/Managers/CatManager.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace GameJamCat
 {
     public class CatManager : MonoBehaviour
     {
-        private ICatFactory _catFactory = null;
-        
+        [SerializeField]
+        private CatGenerator catGenerator;
+
+        List<CatBehaviour> _activeCats;
+
         /// <summary>
         /// Initializes the CatManager
         /// </summary>
         public void Initialize()
         {
-            
+            _activeCats = new List<CatBehaviour>();
         }
 
         /// <summary>
@@ -19,12 +23,26 @@ namespace GameJamCat
         /// </summary>
         public void CleanUp()
         {
-            
+            foreach(var cat in _activeCats)
+            {
+                cat.DestroyCat();
+            }
+            _activeCats.Clear();
+        }
+
+        public CatBehaviour GetRandomCat()
+        {
+            CatBehaviour cat = catGenerator.GetRandomCat();
+            _activeCats.Add(cat);
+            cat.Initialize();
+            return cat;
         }
     }
 
     public interface ICatFactory
     {
         CatBehaviour GetRandomCat();
+
+        void Reclaim(CatBehaviour cat);
     }
 }
