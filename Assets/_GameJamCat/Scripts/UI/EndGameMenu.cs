fa214f262dfd2480e3602db59a5207bdca14b3fe
Martin Tsang <m4rtin.t@gmail.com>
2021-01-30 13:47:47 -0800

    Win Panel with art assets

--- Assets/_GameJamCat/Scripts/UI/EndGameMenu.cs
+++ Assets/_GameJamCat/Scripts/UI/EndGameMenu.cs
@@ -7,33 +7,48 @@ namespace GameJamCat
     public class EndGameMenu : MonoBehaviour
     {
         [SerializeField] private GameObject _loseScreen = null;
-        [SerializeField] private GameObject _winScreen = null;
+        [SerializeField] private WinPanelViewBehaviour _winScreen = null;
         [SerializeField] private Button _loseReplayButton = null;
-        [SerializeField] private Button _winReplayButton = null;
 
-        private bool _foundCat = false; // TempCondition until manager has information
+        public void Initialize()
+        {
+            if (_loseScreen != null)
+            {
+                _loseScreen.SetActive(false);
+            }
+
+            if (_winScreen != null)
+            {
+                _winScreen.gameObject.SetActive(false);
+            }
+
+            if (_loseReplayButton != null)
+            {
+                _loseReplayButton.onClick.AddListener(() => ResetGame());
+            }
+        }
         
         [Button]
         private void WinGame()
         {
-            _foundCat = true;
             StateManager.Instance.SetState(State.EndGame);
         }
         
         [Button]
         private void LoseGame()
         {
-            _foundCat = false;
             StateManager.Instance.SetState(State.EndGame);
         }
         
         public void DisplayEndPanel(bool foundCat)
         { 
-            if (_foundCat)
+            if (foundCat)
             {
                 if (_winScreen != null)
                 {
-                    _winScreen.SetActive(true);
+                    _winScreen.gameObject.SetActive(true);
+                    _winScreen.Initialize();
+                    _winScreen.StartWinAnimation();
                 }
             }
             else
@@ -45,29 +60,7 @@ namespace GameJamCat
             }
         }
 
-        private void Awake()
-        {
-            if (_loseScreen != null)
-            {
-                _loseScreen.SetActive(false);
-            }
 
-            if (_winScreen != null)
-            {
-                _winScreen.SetActive(false);
-            }
-
-            if (_loseReplayButton != null)
-            {
-                _loseReplayButton.onClick.AddListener(() => ResetGame());
-            }
-
-            if (_winReplayButton != null)
-            {
-                _winReplayButton.onClick.AddListener(() => ResetGame());   
-            }
-            
-        }
 
         private void ResetGame()
         {
---
+++ Assets/_GameJamCat/Scripts/UI/WinPanelViewBehaviour.cs
@@ -0,0 +1,53 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameJamCat
+{
+    public class WinPanelViewBehaviour : MonoBehaviour
+    {
+        private const float AnimationPawDistance = 1000;
+
+        [SerializeField] private GameObject _certificate = null;
+        [SerializeField] private Button _winReplayButton = null;
+        
+        [SerializeField]
+        private float _animationDuration = 1.5f;
+
+        [SerializeField] private float _animationCertificateDuration = 1.0f;
+        public void Initialize()
+        {
+            var rectTransform = GetComponent<RectTransform>();
+            //Top
+            rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -AnimationPawDistance);
+            rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, -AnimationPawDistance);
+        }
+
+        /// <summary>
+        /// Starts the Win Game Scenario
+        /// </summary>
+        public void StartWinAnimation()
+        {
+            _certificate.transform.localScale = Vector3.zero;
+            transform.DOBlendableLocalMoveBy(Vector3.up * AnimationPawDistance, _animationDuration, true)
+                .SetEase(Ease.OutQuint).OnComplete( () => 
+            {
+                if (_certificate != null)
+                {
+                    _certificate.transform.DOScale(Vector3.one, _animationCertificateDuration).SetEase(Ease.OutBack);
+                }
+            });
+        }
+
+        private void OnEnable()
+        {
+            _winReplayButton.onClick.RemoveAllListeners();
+            _winReplayButton.onClick.AddListener(ResetGame);
+        }
+        
+        private void ResetGame()
+        {
+            //Add Reset Logic Here
+        }
+    }
+}
