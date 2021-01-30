e37ceeae9e212971327fce8935631af95552420e
Martin Tsang <m4rtin.t@gmail.com>
2021-01-29 20:14:18 -0800

    Beginning Fade screen transition

--- Assets/_GameJamCat/Scripts/Managers/UIManager.cs
+++ Assets/_GameJamCat/Scripts/Managers/UIManager.cs
@@ -1,18 +1,27 @@
+using System;
 using UnityEngine;
 
 namespace GameJamCat
 {
     public class UIManager : MonoBehaviour
     {
-        [SerializeField]
-        private DossierViewBehaviour _dossierView = null;
+        private readonly IStateManager _stateManager = StateManager.Instance;
+        
+        [SerializeField] private DossierViewBehaviour _dossierView = null;
+        [SerializeField] private ScreenTransitionViewBehaviour _transitionViewBehaviour = null;
         
         /// <summary>
         /// Initialize UIManager, setup values here
         /// </summary>
         public void Initialize()
         {
-            _dossierView.Initialize();
+            if (_dossierView != null)
+            {
+                _dossierView.Initialize();
+            }
+            
+            _stateManager.OnStateChanged += HandleOnStateChange;
+            _transitionViewBehaviour.OnCompleteFade += HandleOnFadeComplete;
         }
 
         /// <summary>
@@ -22,5 +31,31 @@ namespace GameJamCat
         {
             
         }
+        
+        #region Delegate
+
+        private void HandleOnStateChange(State state)
+        {
+            switch (state)
+            {
+                case State.Pregame:
+                    _transitionViewBehaviour.SwitchBlackScreen(true);
+                    break;
+                case State.Play:
+                    break;
+                case State.Dialogue:
+                    break;
+                case State.EndGame:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private void HandleOnFadeComplete()
+        {
+            _stateManager.SetState(State.Play);
+        }
+        #endregion
     }
 }
---
+++ Assets/_GameJamCat/Scripts/UI/ScreenTransitionViewBehaviour.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameJamCat
+{
+    public class ScreenTransitionViewBehaviour : MonoBehaviour
+    {
+        [Title("Properties")]
+        [SerializeField] private Image _blackScreen = null;
+        [SerializeField] private float _fadeInDuration = 3;
+
+        public event Action OnCompleteFade;
+        
+        /// <summary>
+        /// Fades In or Out to black with duration
+        /// </summary>
+        /// <param name="isFadingIn">should fade in</param>
+        public void SwitchBlackScreen(bool isFadingIn)
+        {
+            var alphaValue = isFadingIn ? 0 : 1;
+            if (_blackScreen != null)
+            {
+                _blackScreen.CrossFadeAlpha(alphaValue, _fadeInDuration, true);
+                StartCoroutine(FadeCountDown());
+            }
+        }
+
+        private IEnumerator FadeCountDown()
+        {
+            yield return new WaitForSeconds(_fadeInDuration);
+            if (OnCompleteFade != null)
+            {
+                OnCompleteFade();
+            }
+        }
+    }
+}
