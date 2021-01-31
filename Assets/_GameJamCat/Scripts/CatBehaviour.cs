263a91d6c3f5bfd150421fca3a3785aa23fe7d04
Will <williambeltran1997@gmail.com>
2021-01-30 16:19:49 -0800

    Added focusing onto a cat

--- Assets/_GameJamCat/Scripts/CatBehaviour.cs
+++ Assets/_GameJamCat/Scripts/CatBehaviour.cs
@@ -1,23 +1,27 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 namespace GameJamCat
 {
     public class CatBehaviour : MonoBehaviour
     {
         [SerializeField] private Renderer _catRenderer;
-
+        private PlayableDirector playableDirector;
+        private CinemachineVirtualCamera playerVirtualCamera;
 
         public Renderer CatRenderer => _catRenderer;
-
 
         /// <summary>
         /// Called by the CatManager when a cat is grabbed from the pool
         /// </summary>
         public void Initialize(Vector3 spawnPosition)
         {
+            playableDirector = GetComponent<PlayableDirector>();
             transform.position = spawnPosition;
+            SetupVirtualCams();
             gameObject.SetActive(true);
         }
 
@@ -29,6 +33,56 @@ namespace GameJamCat
         private void Update()
         {
 
+        }
+
+        /// <summary>
+        /// Fetches main cam to get cinemachine brain and player cam so we can transition from player cam to cat cam 
+        /// </summary>
+        private void SetupVirtualCams()
+        {
+            foreach (var output in playableDirector.playableAsset.outputs)
+            {
+                if (output.streamName == "Dialouge")
+                {
+                    CinemachineBrain cinemachine = Camera.main.GetComponent<CinemachineBrain>();
+                    CinemachineVirtualCamera playerVirtualCamera = cinemachine.transform.parent.GetComponentInChildren<CinemachineVirtualCamera>();
+                    playableDirector.SetGenericBinding(output.sourceObject, Camera.main.GetComponent<CinemachineBrain>());
+                    var cinemachineTrack = output.sourceObject as CinemachineTrack;
+                    foreach (var clip in cinemachineTrack.GetClips())
+                    {
+                        if (clip.displayName == "Player")
+                        {
+                            var cinemachineShot = clip.asset as CinemachineShot;
+                            playableDirector.SetReferenceValue(cinemachineShot.VirtualCamera.exposedName, playerVirtualCamera);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Begins player Conversation With Cat 
+        /// </summary>
+        public void BeginConversation()
+        {
+            playableDirector.Play();
+        }
+
+        /// <summary>
+        /// Ends conversation with the cat
+        /// </summary>
+        public void EndConversation()
+        {
+            playableDirector.Resume();
+        }
+
+        /// <summary>
+        /// Stops camera animation so we stay on the cat
+        /// </summary>
+        public void StopTimeline()
+        {
+            Debug.Log("=========Pause+======");
+            playableDirector.Pause();
         }
     }
 }
--- Assets/_GameJamCat/Scripts/PlayerController.cs
+++ Assets/_GameJamCat/Scripts/PlayerController.cs
@@ -8,22 +8,29 @@ namespace GameJamCat {
     [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField]
+        private bool _activateControllerDebug = false;
+
         private readonly IStateManager _stateManager = StateManager.Instance;
         
         public CharacterController characterController { get; private set; }
         public PlayerCharacter playerCharacter { get; private set; }
 
         public FirstPersonCamera playerCamera;
+        private Camera _mainCamera;
+        private CatBehaviour _currentCatInFocus = null;
+        private const float _maxReticleDistance = 5f;
 
         protected void Awake()
         {
             characterController = GetComponent<CharacterController>();
             playerCharacter = GetComponent<PlayerCharacter>();
+            _mainCamera = this.transform.parent.GetComponentInChildren<Camera>();
         }
 
         protected void Update()
         {
-            if (_stateManager.GetState() != State.Play)
+            if (_stateManager.GetState() != State.Play && _activateControllerDebug == false)
             {
                 return;
             }
@@ -36,7 +43,7 @@ namespace GameJamCat {
 
             if (playerCharacter)
                 playerCharacter.Simulate(characterController, input);
-
+            FocusObjectUpdate();
         }
 
         protected void LateUpdate()
@@ -57,6 +64,35 @@ namespace GameJamCat {
             float deltaTime = Time.deltaTime;
 
             playerCamera.Simulate(position, rotation, deltaTime);
+        }
+
+        private void FocusObjectUpdate()
+        {
+            Ray ray = _mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, _maxReticleDistance))
+            {
+                if (hit.collider.gameObject.CompareTag("Cat"))
+                {
+                    // Fire other event here that could highlite the cross hair 
+                    // Thas UX babey 
+                    if (Input.GetKeyDown(KeyCode.Mouse1))
+                    {
+                        _currentCatInFocus = hit.collider.gameObject.GetComponent<CatBehaviour>();
+                        _currentCatInFocus.BeginConversation();
+                    }
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        if (_currentCatInFocus == null) return;
+                        _currentCatInFocus.EndConversation();
+                    }
+                }
+            }
+        }
+
+        public void PauseTimelineForCat()
+        {
+            _currentCatInFocus.StopTimeline();
         }
     }
 }
